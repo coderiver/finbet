a2f0f365e71c8705143c56026673b8e53ec476a1
Валерий <kaktuz34@gmail.com>
2015-05-07 23:24:15 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,19 +1,75 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// scroll-pane
+	$('.scroll-pane').jScrollPane();
+	$( window ).resize(function() {
+		var settings = {
+				autoReinitialise: true
+			};
+			var pane = $('.scroll-pane')
+			pane.jScrollPane(settings);
+			var contentPane = pane.data('jsp').getContentPane();
+	});
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// duration-item
+	$('.js-duration-item').on('click', function(){
+		$('.js-duration-item').removeClass('is-active');
+		$(this).addClass('is-active');
+	});
 
-	console.log($('body').html());
+	$('.js-option').each(function () {
+		var thisOption = $(this),
+			dataTime = $(this).data('seconds'),
+			dataMin = $(this).data('munute'),
+			timeSek = dataTime*1000,
+			timeMS = (60000*dataMin)+timeSek,
+			optionDial = thisOption.find('.js-dial'),
+			optionTime = thisOption.find('.js-time'),
+			timeId = optionTime.attr('id');
+		// dial
+		if (optionDial.hasClass('is-yellow')) {
+			var fgColor = "#ffb400";
+		}
+		else {
+			var fgColor = "#fff";
+		}
+		optionDial.knob();
+		optionDial.trigger(
+			'configure',
+			{
+				"width": 14,
+				"height": 14,
+				"min": 0,
+				"max": 100,
+				"fgColor": fgColor,
+				"skin": "tron",
+				"bgColor": "#000",
+				"readOnly": true,
+				"cursor": false
+			}
+		);
+		$({animatedVal: 0}).animate({animatedVal: 100}, {
+			duration: timeMS,
+			easing: "swing",
+			step: function() {
+				optionDial.val(Math.ceil(this.animatedVal)).trigger("change");
+			}
+		});
+		setTimeout(function() {
+			optionDial.parents('.timer__ico').addClass('is-opacity');
+		}, timeMS);
+		// time
+		$('#' + timeId).countDown({
+			targetOffset: {
+				'day': 0,
+				'month': 0,
+				'year': 0,
+				'hour': 0,
+				'min': dataMin,
+				'sec': dataTime
+			},
+			animation: false
+		});
+	});
+
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,7 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/jquery.mousewheel.js",
+					"js/lib/jquery.jscrollpane.min.js",
+					"js/lib/jquery.knob.min.js",
+					"js/lib/jquery.lwtCountdown-1.0.js",
 					"js/common.js");
